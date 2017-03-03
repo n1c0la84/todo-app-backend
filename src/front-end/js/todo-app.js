072f8c9199c42a6fe50530d6b61e796b698321ba
Nicola <nicola084@yahoo.it>
2017-03-03 14:51:24 +0100

    Update todo-app.js

--- src/front-end/js/todo-app.js
+++ src/front-end/js/todo-app.js
@@ -2,4 +2,9 @@ var app = angular.module('todo-app', []);
     app.controller('controller', function($scope, $http) {
         $http.get('http://localhost:4567/todo-app/todos').then(function(resp) {
         $scope.todos = resp.data; });
+        
+        $scope.add = function(contenuto) {
+          $http.post('http://localhost:4567/todo-app/todos', {content: contenuto});
+          window.location.reload();
+        }
 });
